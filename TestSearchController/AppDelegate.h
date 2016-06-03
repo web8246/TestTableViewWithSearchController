39b2c25f5b1a82b234ae7739943fe302958c4f1d
dean <dean@HeredeiMac.local>
2016-06-03 14:38:11 +0800

    first time

---
+++ TestSearchController/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestSearchController
+//
+//  Created by dean on 2016/5/24.
+//  Copyright © 2016年 dean. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestSearchController/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestSearchController
+//
+//  Created by dean on 2016/5/24.
+//  Copyright © 2016年 dean. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
